0c38698190c7b8dc83f43110b48180eda3d673d9
hrabar <karhill103@gmail.com>
2024-06-21 23:59:37 +0200

    init

--- README.md
+++
@@ -1,2 +0,0 @@
-# king-ict-internship-project
-Small Spring Boot project for the King ICT 2024 Summer Internship program
---
+++ src/test/java/com/king_ict_task/rest_api/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.king_ict_task.rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
